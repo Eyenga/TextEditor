1c3fc599181114dc835ca0e11073c95d94a9fa91
Frank Eyenga <eyenga@vt.edu>
2018-02-19 06:28:09 -0500

    Implement "Efficient Document" class. Add code for benchmarking.

--- MOOCTextEditor/src/document/Document.java
+++ MOOCTextEditor/src/document/Document.java
@@ -1,47 +1,58 @@
 package document;
 
-/** 
- * A class that represents a text document
- * @author UC San Diego Intermediate Programming MOOC team
- */
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public abstract class Document {
+/** 
+ * A class that represents a text document
+ * @formatter:off 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Frank Eyenga 
+ * 			Edited: September 28, 2017 
+ * @formatter:on
+ */
+
+public abstract class Document
+{
 
 	private String text;
-	
-	/** Create a new document from the given text.
-	 * Because this class is abstract, this is used only from subclasses.
-	 * @param text The text of the document.
+
+	/**
+	 * Create a new document from the given text. Because this class is abstract,
+	 * this is used only from subclasses.
+	 * 
+	 * @param text
+	 *            The text of the document.
 	 */
 	protected Document(String text)
 	{
 		this.text = text;
 	}
-	
-	/** Returns the tokens that match the regex pattern from the document 
-	 * text string.
-	 * @param pattern A regular expression string specifying the 
-	 *   token pattern desired
-	 * @return A List of tokens from the document text that match the regex 
-	 *   pattern
+
+	/**
+	 * Returns the tokens that match the regex pattern from the document text
+	 * string.
+	 * 
+	 * @param pattern
+	 *            A regular expression string specifying the token pattern desired
+	 * @return A List of tokens from the document text that match the regex pattern
 	 */
 	protected List<String> getTokens(String pattern)
 	{
 		ArrayList<String> tokens = new ArrayList<String>();
 		Pattern tokSplitter = Pattern.compile(pattern);
 		Matcher m = tokSplitter.matcher(text);
-		
-		while (m.find()) {
+
+		while (m.find())
+		{
 			tokens.add(m.group());
 		}
-		
+
 		return tokens;
 	}
-	
+
 	/** This is a helper function that returns the number of syllables
 	 * in a word.  You should write this and use it in your 
 	 * BasicDocument class.
@@ -57,26 +68,75 @@ public abstract class Document {
 	 * 
 	 * @param word  The word to count the syllables in
 	 * @return The number of syllables in the given word, according to 
-	 * this rule: Each contiguous sequence of one or more vowels is a syllable, 
+	 * this rule: 
+	 * @formatter:off
+	 * 		 Each contiguous sequence of one or more vowels is a syllable, 
 	 *       with the following exception: a lone "e" at the end of a word 
 	 *       is not considered a syllable unless the word has no other syllables. 
-	 *       You should consider y a vowel.
+	 *       You should consider y a vowel. 
+	 * @formatter:on
 	 */
 	protected int countSyllables(String word)
 	{
-		// TODO: Implement this method so that you can call it from the 
-	    // getNumSyllables method in BasicDocument (module 2) and 
-	    // EfficientDocument (module 3).
-	    return 0;
+		int syllables = 0;
+		word = word.toLowerCase();
+		int length = word.length();
+
+		for (int i = 0; i < length; i++)
+		{
+			char c = word.charAt(i);
+
+			if (isVowel(c))
+			{
+				switch (c)
+				{
+				case 'e':
+					if ((i == (length - 1)) && (syllables != 0))
+					{
+						continue;
+					}
+				default:
+					if ((i != 0) && isVowel(word.charAt(i - 1)))
+					{
+						continue;
+					}
+					break;
+				}
+
+				syllables++;
+			}
+		}
+		return syllables;
 	}
-	
-	/** A method for testing
+
+	private boolean isVowel(char c)
+	{
+		switch (c)
+		{
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'y':
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	/**
+	 * A method for testing
 	 * 
-	 * @param doc The Document object to test
-	 * @param syllables The expected number of syllables
-	 * @param words The expected number of words
-	 * @param sentences The expected number of sentences
-	 * @return true if the test case passed.  False otherwise.
+	 * @param doc
+	 *            The Document object to test
+	 * @param syllables
+	 *            The expected number of syllables
+	 * @param words
+	 *            The expected number of words
+	 * @param sentences
+	 *            The expected number of sentences
+	 * @return true if the test case passed. False otherwise.
 	 */
 	public static boolean testCase(Document doc, int syllables, int words, int sentences)
 	{
@@ -86,55 +146,53 @@ public abstract class Document {
 		int syllFound = doc.getNumSyllables();
 		int wordsFound = doc.getNumWords();
 		int sentFound = doc.getNumSentences();
-		if (syllFound != syllables) {
-			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
-					+ ", expected " + syllables);
+		if (syllFound != syllables)
+		{
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound + ", expected " + syllables);
 			passed = false;
 		}
-		if (wordsFound != words) {
-			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
-					+ ", expected " + words);
+		if (wordsFound != words)
+		{
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound + ", expected " + words);
 			passed = false;
 		}
-		if (sentFound != sentences) {
-			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
-					+ ", expected " + sentences);
+		if (sentFound != sentences)
+		{
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound + ", expected " + sentences);
 			passed = false;
 		}
-		
-		if (passed) {
+
+		if (passed)
+		{
 			System.out.println("passed.\n");
-		}
-		else {
+		} else
+		{
 			System.out.println("FAILED.\n");
 		}
 		return passed;
 	}
-	
-	
+
 	/** Return the number of words in this document */
 	public abstract int getNumWords();
-	
+
 	/** Return the number of sentences in this document */
 	public abstract int getNumSentences();
-	
+
 	/** Return the number of syllables in this document */
 	public abstract int getNumSyllables();
-	
+
 	/** Return the entire text of this document */
 	public String getText()
 	{
 		return this.text;
 	}
-	
+
 	/** return the Flesch readability score of this document */
 	public double getFleschScore()
 	{
-	    // TODO: You will play with this method in week 1, and 
-		// then implement it in week 2
-	    return 0.0;
+		double numOfWords = getNumWords(), numOfSentences = getNumSentences(), numOfSyllables = getNumSyllables();
+
+		return 206.835 - (1.015 * numOfWords / numOfSentences) - (84.6 * numOfSyllables / numOfWords);
 	}
-	
-	
-	
+
 }
--- MOOCTextEditor/src/document/DocumentBenchmarking.java
+++ MOOCTextEditor/src/document/DocumentBenchmarking.java
@@ -4,97 +4,106 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
-/** A class for timing the EfficientDocument and BasicDocument classes
+/**
+ * A class for timing the EfficientDocument and BasicDocument classes
  * 
  * @author UC San Diego Intermediate Programming MOOC team
  *
  */
 
-public class DocumentBenchmarking {
+public class DocumentBenchmarking
+{
 
-	
-	public static void main(String [] args) {
+	public static void main(String[] args)
+	{
 
-	    // Run each test more than once to get bigger numbers and less noise.
-	    // You can try playing around with this number.
-	    int trials = 100;
+		// Run each test more than once to get bigger numbers and less noise.
+		// You can try playing around with this number.
+		int trials = 100;
 
-	    // The text to test on
-	    String textfile = "data/warAndPeace.txt";
-		
-	    // The amount of characters to increment each step
-	    // You can play around with this
+		// The text to test on
+		String textfile = "data/warAndPeace.txt";
+
+		// The amount of characters to increment each step
+		// You can play around with this
 		int increment = 20000;
 
-		// The number of steps to run.  
+		// The number of steps to run.
 		// You can play around with this.
-		int numSteps = 20;
-		
-		// THe number of characters to start with. 
+		int numSteps = 25;
+
+		// THe number of characters to start with.
 		// You can play around with this.
 		int start = 50000;
-		
-		// TODO: Fill in the rest of this method so that it runs two loops
-		// and prints out timing results as described in the assignment 
-		// instructions and following the pseudocode below.
-		for (int numToCheck = start; numToCheck < numSteps*increment + start; 
-				numToCheck += increment)
+
+		System.out.printf("%1$-15s\t%2$-10s\tEfficient Time%n", "Characters Read", "Naive Time");
+
+		for (int numToCheck = start; numToCheck < numSteps * increment + start; numToCheck += increment)
 		{
-			// numToCheck holds the number of characters that you should read from the 
-			// file to create both a BasicDocument and an EfficientDocument.  
-			
-			/* Each time through this loop you should:
-			 * 1. Print out numToCheck followed by a tab (\t) (NOT a newline)
-			 * 2. Read numToCheck characters from the file into a String
-			 *     Hint: use the helper method below.
-			 * 3. Time a loop that runs trials times (trials is the variable above) that:
-			 *     a. Creates a BasicDocument 
-			 *     b. Calls fleshScore on this document
-			 * 4. Print out the time it took to complete the loop in step 3 
-			 *      (on the same line as the first print statement) followed by a tab (\t)
-			 * 5. Time a loop that runs trials times (trials is the variable above) that:
-			 *     a. Creates an EfficientDocument 
-			 *     b. Calls fleshScore on this document
-			 * 6. Print out the time it took to complete the loop in step 5 
-			 *      (on the same line as the first print statement) followed by a newline (\n) 
-			 */  
-			 
+			String text = getStringFromFile(textfile, numToCheck);
+			long start_time;
+			double basic_time, efficient_time;
+
+			start_time = System.nanoTime();
+			for (int i = 0; i <= trials; i++)
+			{
+				BasicDocument test = new BasicDocument(text);
+				test.getFleschScore();
+			}
+
+			basic_time = (System.nanoTime() - start_time) / 0x3B9ACA00;
+
+			start_time = System.nanoTime();
+			for (int i = 0; i <= trials; i++)
+			{
+				EfficientDocument test = new EfficientDocument(text);
+				test.getFleschScore();
+			}
+			efficient_time = (System.nanoTime() - start_time) / 0x3B9ACA00;
+
+			System.out.printf("%1$-15d\t%2$-10f\t%3$f%n", numToCheck, basic_time, efficient_time);
 		}
-	
+
 	}
-	
-	/** Get a specified number of characters from a text file
+
+	/**
+	 * Get a specified number of characters from a text file
 	 * 
-	 * @param filename The file to read from
-	 * @param numChars The number of characters to read
-	 * @return The text string from the file with the appropriate number of characters
+	 * @param filename
+	 *            The file to read from
+	 * @param numChars
+	 *            The number of characters to read
+	 * @return The text string from the file with the appropriate number of
+	 *         characters
 	 */
-	public static String getStringFromFile(String filename, int numChars) {
-		
+	public static String getStringFromFile(String filename, int numChars)
+	{
+
 		StringBuffer s = new StringBuffer();
-		try {
-			FileInputStream inputFile= new FileInputStream(filename);
+		try
+		{
+			FileInputStream inputFile = new FileInputStream(filename);
 			InputStreamReader inputStream = new InputStreamReader(inputFile);
 			BufferedReader bis = new BufferedReader(inputStream);
 			int val;
 			int count = 0;
-			while ((val = bis.read()) != -1 && count < numChars) {
-				s.append((char)val);
+			while ((val = bis.read()) != -1 && count < numChars)
+			{
+				s.append((char) val);
 				count++;
 			}
-			if (count < numChars) {
+			if (count < numChars)
+			{
 				System.out.println("Warning: End of file reached at " + count + " characters.");
 			}
 			bis.close();
-		}
-		catch(Exception e)
+		} catch (Exception e)
 		{
-		  System.out.println(e);
-		  System.exit(0);
+			System.out.println(e);
+			System.exit(0);
 		}
-		
-		
+
 		return s.toString();
 	}
-	
+
 }
